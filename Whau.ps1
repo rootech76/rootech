b164fe0643cee8da91733c04017cbfc6778221f2
RooTech <rootech76@gmail.com>
2026-04-07 12:25:41 -0300

    Estructurando...

--- Whau.ps1
+++ Whau.ps1
@@ -1,1 +1,41 @@
 #Whau
+
+################## INICIALICACIÓN DE VARIABLES #################### 
+
+# HardWare *********************************************************
+$MBoard = Get-CimInstance win32_BaseBoard
+$CPU = Get-CimInstance win32_Processor
+$BIOS = Get-CimInstance win32_BIOS 
+$GPU = Get-CimInstance win32_Videocontroller
+$RAM = Get-CimInstance win32_PhysicalMemory
+
+#SoftWare ***********************************************************
+
+$OS = Get-cimInstance win32_OperatingSystem
+
+
+
+
+Clear-Host
+
+Write-Host ("WHAU") 
+Write-Host ("******** HARDWARE ************")
+Write-Host ("Datos de la Placa Base: ")
+
+
+Write-Host ("Datos del BIOS : ")
+
+Write-Host ("Datos del CPU :")
+
+Write-Host ("Datos de la GPU: ")
+
+Write-Host ("Datos de la RAM: ")
+
+Write-Host ("******** SOFTWARE ************")
+Write-Host ("Datos del Sistema Operitvo: ")
+
+Write-Host "Precione Enter para Salir: " -NoNewLine
+Read-Host 
+Clear-Host
+
+
